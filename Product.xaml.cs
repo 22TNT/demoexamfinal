d3dc012dccfa3a834ca856ab17d54e09ece68c27
22TNT <80966455+22TNT@users.noreply.github.com>
2022-11-28 19:15:01 +0700

    Add files via upload

---
+++ Product.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace de28
+{
+    /// <summary>
+    /// Логика взаимодействия для Product.xaml
+    /// </summary>
+    public partial class Product : UserControl
+    {
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductManufacturer { get; set; }
+        public string ProductImagePath { get; set; } = @"Images/picture.png";
+        public string ProductCountType { get; set; }
+        public float ProductPrice { get; set; }
+        public int ProductCount { get; set; }
+        public Product()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
